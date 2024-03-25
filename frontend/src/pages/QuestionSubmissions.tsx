c820fb5a7db4ae197045efebc2994afdbb81d43a
xlzior <wenjun.lye@gmail.com>
2024-03-25 13:13:53 +0800

    Display query in submissions history

--- frontend/src/pages/QuestionSubmissions.tsx
+++ frontend/src/pages/QuestionSubmissions.tsx
@@ -1,10 +1,53 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Badge, Stack, Table, Title } from '@mantine/core';
+import {
+  Badge,
+  Button,
+  Code,
+  Popover,
+  Stack,
+  Table,
+  Text,
+  Title,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { notifications } from '@mantine/notifications';
+import { IconCopy } from '@tabler/icons-react';
 
 import { useUserStore } from '../store/userStore';
 import type { SubmissionDto } from '../types/question';
 import { QuestionStatus, getSubmissions } from '../api/question';
+
+function QueryDisplay({ query }: { query: string }) {
+  const [opened, { close, open }] = useDisclosure(false);
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(query);
+    notifications.show({ message: 'Copied to clipboard' });
+  };
+
+  return (
+    <Popover withArrow shadow="lg" opened={opened}>
+      <Popover.Target>
+        <Button
+          variant="transparent"
+          onMouseEnter={open}
+          onMouseLeave={close}
+          p={0}
+          rightSection={<IconCopy color="grey" />}
+          onClick={handleClick}
+        >
+          <Text truncate maw="300px">
+            <Code>{query}</Code>
+          </Text>
+        </Button>
+      </Popover.Target>
+      <Popover.Dropdown style={{ pointerEvents: 'none' }}>
+        <Code styles={{ root: { fontSize: 16 } }}>{query}</Code>
+      </Popover.Dropdown>
+    </Popover>
+  );
+}
 
 export default function QuestionSubmissions() {
   const { profile } = useUserStore();
@@ -22,6 +65,9 @@ export default function QuestionSubmissions() {
 
   const rows = submissions.map((submission) => (
     <Table.Tr key={submission.submission_time.toString()}>
+      <Table.Td>
+        <QueryDisplay query={submission.query} />
+      </Table.Td>
       <Table.Td>{submission.submission_time.toISOString()}</Table.Td>
       <Table.Td>
         <Badge
@@ -69,6 +115,7 @@ export default function QuestionSubmissions() {
       >
         <Table.Thead>
           <Table.Tr>
+            <Table.Th>Query</Table.Th>
             <Table.Th>Submission Time</Table.Th>
             <Table.Th>Status</Table.Th>
             <Table.Th>Correctness</Table.Th>
