07907b5afe3309da150a716a14909cea4abcc796
Taufiq Bin Abdul Rahman <taufiq.b.ar@gmail.com>
2024-03-27 23:01:49 +0800

    add call api from home page

--- frontend/src/api/questions.ts
+++ frontend/src/api/questions.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { QuestionDto } from '../types/question';
 
+const API_URL = process.env.REACT_APP_API_URL || 'localhost:3000';
+
 export const getQuestions = async (page: number = 1, limit: number = 10) => {
   const resp = await axios.get<QuestionDto[]>(
-    `/question?page=${page}&limit=${limit}`,
+    `${API_URL}/question?page=${page}&limit=${limit}`,
   );
   return resp.data;
 };
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,103 +1,22 @@
 import { Anchor, Flex, Pagination, Table, Title } from '@mantine/core';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ROUTES } from '../constants/routes';
 import { QuestionDto } from '../types/question';
+import { getQuestions } from '../api/questions';
 
 const HomePage = () => {
   const ITEMS_PER_PAGE = 10;
   const [currentPage, setCurrentPage] = useState(1);
-  const questions: QuestionDto[] = [
-    {
-      id: 1,
-      name: 'Question 1',
-      description: 'Description for Question 1',
-      question_schema: 'Schema for Question 1',
-      question_data: 'Data for Question 1',
-      answer_data: 'Answer Data for Question 1',
-      max_timeout: 60,
-    },
-    {
-      id: 2,
-      name: 'Question 2',
-      description: 'Description for Question 2',
-      question_schema: 'Schema for Question 2',
-      question_data: 'Data for Question 2',
-      answer_data: 'Answer Data for Question 2',
-      max_timeout: 120,
-    },
-    {
-      id: 3,
-      name: 'Question 3',
-      description: 'Description for Question 3',
-      question_schema: 'Schema for Question 3',
-      question_data: 'Data for Question 3',
-      answer_data: 'Answer Data for Question 3',
-      max_timeout: 90,
-    },
-    {
-      id: 4,
-      name: 'Question 4',
-      description: 'Description for Question 4',
-      question_schema: 'Schema for Question 4',
-      question_data: 'Data for Question 4',
-      answer_data: 'Answer Data for Question 4',
-      max_timeout: 45,
-    },
-    {
-      id: 5,
-      name: 'Question 5',
-      description: 'Description for Question 5',
-      question_schema: 'Schema for Question 5',
-      question_data: 'Data for Question 5',
-      answer_data: 'Answer Data for Question 5',
-      max_timeout: 80,
-    },
-    {
-      id: 6,
-      name: 'Question 6',
-      description: 'Description for Question 6',
-      question_schema: 'Schema for Question 6',
-      question_data: 'Data for Question 6',
-      answer_data: 'Answer Data for Question 6',
-      max_timeout: 150,
-    },
-    {
-      id: 7,
-      name: 'Question 7',
-      description: 'Description for Question 7',
-      question_schema: 'Schema for Question 7',
-      question_data: 'Data for Question 7',
-      answer_data: 'Answer Data for Question 7',
-      max_timeout: 100,
-    },
-    {
-      id: 8,
-      name: 'Question 8',
-      description: 'Description for Question 8',
-      question_schema: 'Schema for Question 8',
-      question_data: 'Data for Question 8',
-      answer_data: 'Answer Data for Question 8',
-      max_timeout: 30,
-    },
-    {
-      id: 9,
-      name: 'Question 9',
-      description: 'Description for Question 9',
-      question_schema: 'Schema for Question 9',
-      question_data: 'Data for Question 9',
-      answer_data: 'Answer Data for Question 9',
-      max_timeout: 70,
-    },
-    {
-      id: 10,
-      name: 'Question 10',
-      description: 'Description for Question 10',
-      question_schema: 'Schema for Question 10',
-      question_data: 'Data for Question 10',
-      answer_data: 'Answer Data for Question 10',
-      max_timeout: 110,
-    },
-  ];
+  const [questions, setQuestions] = useState<QuestionDto[]>([]);
+  useEffect(() => {
+    getQuestions(currentPage)
+      .then((data) => {
+        setQuestions(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching questions:', error);
+      });
+  }, []);
 
   const rows = questions.map((qn) => (
     <Table.Tr key={qn.id}>
