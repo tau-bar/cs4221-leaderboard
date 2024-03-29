37936c82450503c7cb01c2ef1effc1ac5778c9f6
tyanhan <tayyanhanw@gmail.com>
2024-03-29 15:33:47 +0800

    Uncomment migration

--- backend/db/migrations/1711560036499-AddLeTour.ts
+++ backend/db/migrations/1711560036499-AddLeTour.ts
@@ -1,19 +1,19 @@
-// import { leTourData, leTourSchema } from "db/question_datasets/letour";
-// import { MigrationInterface, QueryRunner } from "typeorm";
+import { leTourData, leTourSchema } from "db/question_datasets/letour";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-// export class AddLeTour1711560036499 implements MigrationInterface {
-//     public async up(queryRunner: QueryRunner): Promise<void> {
-//         await queryRunner.query(`CREATE SCHEMA IF NOT EXISTS letour`);
-//         await queryRunner.query(`GRANT USAGE ON SCHEMA letour TO ${process.env.PARTICIPANT_USERNAME};`)
-//         await queryRunner.query(`ALTER DEFAULT PRIVILEGES FOR ROLE ${process.env.ADMIN_USERNAME} IN SCHEMA letour GRANT SELECT ON TABLES TO ${process.env.PARTICIPANT_USERNAME}`);
-//         const modifiedTables = leTourSchema.replace(/CREATE TABLE IF NOT EXISTS (\w+)/g, 'CREATE TABLE IF NOT EXISTS "letour"."$1"').replace(/REFERENCES (\w+)/g, 'REFERENCES "letour"."$1"');
-//         await queryRunner.query(modifiedTables); // Execute each query separately
+export class AddLeTour1711560036499 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE SCHEMA IF NOT EXISTS letour`);
+        await queryRunner.query(`GRANT USAGE ON SCHEMA letour TO ${process.env.PARTICIPANT_USERNAME};`)
+        await queryRunner.query(`ALTER DEFAULT PRIVILEGES FOR ROLE ${process.env.ADMIN_USERNAME} IN SCHEMA letour GRANT SELECT ON TABLES TO ${process.env.PARTICIPANT_USERNAME}`);
+        const modifiedTables = leTourSchema.replace(/CREATE TABLE IF NOT EXISTS (\w+)/g, 'CREATE TABLE IF NOT EXISTS "letour"."$1"').replace(/REFERENCES (\w+)/g, 'REFERENCES "letour"."$1"');
+        await queryRunner.query(modifiedTables); // Execute each query separately
 
-//         const modifiedData = leTourData.replace(/INSERT INTO (\w+)/g, 'INSERT INTO "letour"."$1"');
-//         await queryRunner.query(modifiedData);
-//     }
+        const modifiedData = leTourData.replace(/INSERT INTO (\w+)/g, 'INSERT INTO "letour"."$1"');
+        await queryRunner.query(modifiedData);
+    }
 
-//     public async down(queryRunner: QueryRunner): Promise<void> {
-//         await queryRunner.query(`DROP SCHEMA letour CASCADE`);
-//     }
-// }
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP SCHEMA letour CASCADE`);
+    }
+}
